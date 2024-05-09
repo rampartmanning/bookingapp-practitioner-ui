6b94a4a4916ad03199f469ff0f4c542e810dac5d
dgarrard <david@konfidis.com>
2024-05-09 08:58:17 -0400

    Add cache invalidation config to deploy script (prod)

--- public/bookingapp-practitioner-ui/deploy_prod.sh
+++ public/bookingapp-practitioner-ui/deploy_prod.sh
@@ -1,6 +1,7 @@
 cp settings/settings-prod.js src/settings.js
 npm run build
 aws s3 sync ./dist s3://bookingapp-practitioner-ui-prod
+aws cloudfront create-invalidation --distribution-id E2A70KSM0V6IKE --paths "/*" > /dev/null 2>&1
 # reset to back to dev version for local development
 cp settings/settings-dev.js src/settings.js
 echo "DONE: Deployed to prod environment"
