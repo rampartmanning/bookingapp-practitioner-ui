d6bb3baa9f6debcbbe56d96f9e2946f8c103ab4d
dgarrard <david@konfidis.com>
2024-06-12 09:38:41 -0400

    Fix date formatting issues for timezones.

--- public/bookingapp-practitioner-ui/src/views/CodeLookupView.vue
+++ public/bookingapp-practitioner-ui/src/views/CodeLookupView.vue
@@ -85,7 +85,7 @@
                   </tr>
                   <tr v-if="booking_practitioner.current_status == 'waiting_for_practitioner'">
                     <td>Preferred Service Date</td>
-                    <td>{{ formatDate(booking.preferred_service_date, "PP") }}</td>
+                    <td>{{ formatDate(booking.preferred_service_date_dt, "PP") }}</td>
                   </tr>                
                   <tr v-if="booking_practitioner.current_status == 'waiting_for_practitioner'">
                     <td>Scheduling Preferences</td>
@@ -377,6 +377,8 @@ export default {
             this.booking_practitioner.expires_at = new Date(this.booking_practitioner.expires_at);
           }
           this.booking = response.data.booking;
+          this.booking.preferred_service_date_dt = new Date(this.booking.preferred_service_date);
+          this.booking.preferred_service_date_dt = new Date(this.booking.preferred_service_date_dt.getTime() + (this.booking.preferred_service_date_dt.getTimezoneOffset() * 60000));
           this.mapCenter.lat = this.booking.lat;
           this.mapCenter.lng = this.booking.lng;
           this.mapMarkerOptions.position.lat = this.booking.lat;
